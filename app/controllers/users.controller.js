b2350d142b09d2484ca5094a2af9382eedf41ac1
Michael Woodard <mdw85@uclive.ac.nz>
2021-03-29 17:21:47 +1300

    Completed all methods for users

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,4 +1,6 @@
 const users = require('../models/users.model');
+const bcrypt = require("bcrypt");
+
 exports.register = async function(req, res){
     try {
         const firstName = req.body.firstName;
@@ -9,11 +11,11 @@ exports.register = async function(req, res){
 
         let checker = 0;
 
-        if((firstName == null || firstName == "") || (lastName == null || lastName == "") || (email == null || !/.+@.+$/.test(email) || emailCheck.length > 0) || (password == null || password == "")){
+        if((firstName == null || firstName === "") || (lastName == null || lastName === "") || (email == null || !/^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+$/.test(email) || emailCheck.length > 0) || (password == null || password === "")){
             checker = 1;
         }
 
-        if (checker == 1) {
+        if (checker === 1) {
             res.statusMessage = "Bad Request";
             res.status(400).send();
         }
@@ -30,17 +32,160 @@ exports.register = async function(req, res){
 };
 
 exports.login = async function(req, res){
-    return null;
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const userCheck = await users.getEmail(email);
+
+        let checker = 0;
+
+        if (userCheck.length === 0) {
+            checker = 1;
+        }
+
+        if (checker === 1) {
+            res.statusMessage = "Bad Request";
+            res.status(400).send();
+        }
+        else {
+            const emailCheck = userCheck[0].email;
+            const passwordCheck = userCheck[0].password;
+            const validPassword = await bcrypt.compare(password, passwordCheck);
+
+            if (!validPassword || email !== emailCheck){
+                res.statusMessage = "Bad Request";
+                res.status(400).send();
+            } else {
+                const id = userCheck[0].id;
+                const token = await users.setToken(id);
+                res.statusMessage = "OK";
+                res.status(200).send({userId: id, token: token});
+                }
+            }
+        } catch( err ) {
+            console.log(err);
+            res.statusMessage = "Internal Server Error";
+            res.status(500).send();
+    }
 };
 
 exports.logout = async function(req, res){
-    return null;
+    try {
+        const token = req.header('X-Authorization');
+        const userCheck = await users.findToken(token);
+
+        let checker = 0;
+
+        if (userCheck.length === 0) {
+            checker = 1;
+        }
+
+        if (checker === 1) {
+            res.statusMessage = "Unauthorized";
+            res.status(401).send();
+        } else {
+            const id = userCheck[0].id;
+            await users.logout(id);
+            res.statusMessage = "OK";
+            res.status(200).send();
+        }
+    } catch( err ) {
+        console.log(err);
+        res.statusMessage = "Internal Server Error";
+        res.status(500).send();
+    }
 };
 
 exports.read = async function(req, res){
-    return null;
+    try {
+        const id = req.params.id;
+        const userCheck = await users.getUser(id);
+
+        let checker = 0;
+
+        if (userCheck.length === 0) {
+            checker = 1;
+        }
+
+        if (checker === 1) {
+            res.statusMessage = "Not Found";
+            res.status(404).send();
+        } else {
+            const token = req.header('X-Authorization');
+            if (userCheck[0].auth_token === token) {
+                const result = {firstName: userCheck[0].first_name, lastName: userCheck[0].last_name, email: userCheck[0].email};
+                res.statusMessage = "OK";
+                res.status(200).send(result);
+            } else {
+                const result = {firstName: userCheck[0].first_name, lastName: userCheck[0].last_name};
+                res.statusMessage = "OK";
+                res.status(200).send(result);
+            }
+        }
+    } catch( err ) {
+        console.log(err);
+        res.statusMessage = "Internal Server Error";
+        res.status(500).send();
+    }
 };
 
 exports.update = async function(req, res){
-    return null;
+    try {
+        const id = req.params.id;
+        const userCheck = await users.getUser(id);
+        const token = req.header('X-Authorization');
+            if (token !== userCheck[0].auth_token) {
+                res.statusMessage = "Unauthorized";
+                res.status(401).send();
+            } else {
+                const firstName = req.body.firstName;
+                const lastName = req.body.lastName;
+                const email = req.body.email;
+                const currentPassword = req.body.currentPassword;
+                const password = req.body.password;
+
+                if (password != null) {
+                    const validPassword = await bcrypt.compare(currentPassword, userCheck[0].password);
+                    if (!validPassword) {
+                        res.statusMessage = "Forbidden";
+                        res.status(403).send();
+                    } else {
+                        await users.setPassword(id, password);
+                    }
+                }
+
+                if (firstName != null) {
+                    if (firstName === "") {
+                        res.statusMessage = "Bad Request";
+                        res.status(400).send();
+                    } else {
+                        await users.setFirstName(id,firstName);
+                    }
+                }
+
+                if (lastName != null) {
+                    if (lastName === "") {
+                        res.statusMessage = "Bad Request";
+                        res.status(400).send();
+                    } else {
+                        await users.setLastName(id, lastName);
+                    }
+                }
+
+                if (email != null) {
+                    if (!/^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+$/.test(email)) {
+                        res.statusMessage = "Bad Request";
+                        res.status(400).send();
+                    } else {
+                        await users.setEmail(id, email);
+                    }
+                }
+                res.statusMessage = "OK";
+                res.status(200).send();
+            }
+    } catch( err ) {
+        console.log(err);
+        res.statusMessage = "Internal Server Error";
+        res.status(500).send();
+    }
 };
--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,9 +1,13 @@
 const db = require('../../config/db');
+const bcrypt = require("bcrypt");
+const crypto = require('crypto');
 
 exports.register = async function(firstName, lastName, email, password) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
     const conn = await db.getPool().getConnection();
     const query = 'insert into user (first_name, last_name, email, password) values (?)';
-    const [result] = await conn.query(query, [[firstName, lastName, email, password]]);
+    const [result] = await conn.query(query, [[firstName, lastName, email, hash]]);
     conn.release();
     return result;
 };
@@ -14,4 +18,65 @@ exports.getEmail = async function(email) {
     const [result] = await conn.query( query, [email] );
     conn.release();
     return result;
+};
+
+exports.setToken = async function(id) {
+    const conn = await db.getPool().getConnection();
+    const token = await crypto.randomBytes(16).toString('base64');
+    const query = 'update user set auth_token = ? where id = ?';
+    await conn.query(query, [token, id]);
+    conn.release();
+    return token;
+};
+
+exports.findToken = async function(token) {
+    const conn = await db.getPool().getConnection();
+    const query = 'select * from user where auth_token = ?';
+    const [result] = await conn.query( query, [token] );
+    conn.release();
+    return result;
+};
+
+exports.logout = async function(id) {
+    const conn = await db.getPool().getConnection();
+    const query = 'update user set auth_token = null where id = ?';
+    await conn.query(query, [id]);
+    conn.release();
+};
+
+exports.getUser = async function(id) {
+    const conn = await db.getPool().getConnection();
+    const query = 'select * from user where id = ?';
+    const [result] = await conn.query( query, [id] );
+    conn.release();
+    return result;
+};
+
+exports.setFirstName = async function(id, firstName) {
+    const conn = await db.getPool().getConnection();
+    const query = 'update user set first_name = ? where id = ?';
+    await conn.query(query, [firstName, id]);
+    conn.release();
+};
+
+exports.setLastName = async function(id, lastName) {
+    const conn = await db.getPool().getConnection();
+    const query = 'update user set last_name = ? where id = ?';
+    await conn.query(query, [lastName, id]);
+    conn.release();
+};
+
+exports.setEmail = async function(id, email) {
+    const conn = await db.getPool().getConnection();
+    const query = 'update user set email = ? where id = ?';
+    await conn.query(query, [email, id]);
+    conn.release();
+};
+exports.setPassword = async function(id, password) {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    const conn = await db.getPool().getConnection();
+    const query = 'update user set password = ? where id = ?';
+    await conn.query(query, [hash, id]);
+    conn.release();
 };
